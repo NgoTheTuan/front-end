439f6e865fd037258389da891cf4df5d9efdc7b1
Ngo The Tuan <ngothetuan27082001@gmail.com>
2022-11-15 09:51:51 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './pages/Home/Home';
+import Login from './pages/login/Login';
+import Profile from './pages/profile/Profile';
+import Register from './pages/register/Register';
+import Messenger from './pages/messenger/Messenger';
+
+import RequireAuth from './components/requireAuth/RequireAuth';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route
+                    path="/"
+                    element={
+                        <RequireAuth>
+                            <Home />
+                        </RequireAuth>
+                    }
+                />
+                <Route
+                    path="/profile/:userId"
+                    element={
+                        <RequireAuth>
+                            <Profile />
+                        </RequireAuth>
+                    }
+                />
+                <Route
+                    path="/messenger"
+                    element={
+                        <RequireAuth>
+                            <Messenger />
+                        </RequireAuth>
+                    }
+                />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
